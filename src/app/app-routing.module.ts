80fe550290361a8ab76282db8700b7bbc17d2b14
KBCHANUKAJ <chanukakbc@gmail.com>
2023-10-27 16:16:54 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AdminDashComponent } from './admin-dash/admin-dash.component';
 import { StudentDashComponent } from './student-dash/student-dash.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
 
 
 const routes: Routes = [
   {path:"",component:LoginComponent},
   {path:"admin-dash",component:AdminDashComponent},
-  {path:"student-dash",component:StudentDashComponent}
+  {path:"student-dash",component:StudentDashComponent},
+  {path:"nav-bar",component:NavBarComponent}
   
 ];
 
