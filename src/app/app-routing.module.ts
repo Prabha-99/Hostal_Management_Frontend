65cbad22a91f84953f69eaaf3def7c260f3d530a
KBCHANUKAJ <chanukakbc@gmail.com>
2023-10-27 16:35:52 +0530

    create admin side component and nav bar complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { LoginComponent } from './login/login.component';
 import { AdminDashComponent } from './admin-dash/admin-dash.component';
 import { StudentDashComponent } from './student-dash/student-dash.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AdminSideComponent } from './admin-side/admin-side.component';
 
 
 
@@ -15,7 +16,8 @@ const routes: Routes = [
   {path:"",component:LoginComponent},
   {path:"admin-dash",component:AdminDashComponent},
   {path:"student-dash",component:StudentDashComponent},
-  {path:"nav-bar",component:NavBarComponent}
+  {path:"nav-bar",component:NavBarComponent},
+  {path:"admin-side",component:AdminSideComponent}
   
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,8 @@ import { LoginComponent } from './login/login.component';
 import { AdminDashComponent } from './admin-dash/admin-dash.component';
 import { StudentDashComponent } from './student-dash/student-dash.component';
 import { HttpClientModule } from '@angular/common/http';
-import { NavBarComponent } from './nav-bar/nav-bar.component'; 
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AdminSideComponent } from './admin-side/admin-side.component'; 
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     LoginComponent,
     AdminDashComponent,
     StudentDashComponent,
-    NavBarComponent
+    NavBarComponent,
+    AdminSideComponent
   ],
   imports: [
     FormsModule,
