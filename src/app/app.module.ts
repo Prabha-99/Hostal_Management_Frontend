30d659383b64f644aa74810b020669d273575a6b
KBCHANUKAJ <chanukakbc@gmail.com>
2023-10-27 16:15:46 +0530

    Create Nav Bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,16 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AdminDashComponent } from './admin-dash/admin-dash.component';
 import { StudentDashComponent } from './student-dash/student-dash.component';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
+import { NavBarComponent } from './nav-bar/nav-bar.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     AdminDashComponent,
-    StudentDashComponent
+    StudentDashComponent,
+    NavBarComponent
   ],
   imports: [
     FormsModule,
