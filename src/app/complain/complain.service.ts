ff48ed0ee0cd266814efd4913e8228d397aaa09d
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-11-01 14:43:09 +0530

    Added Complain From to the Project

---
+++ src/app/complain/complain.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ComplainService {
+
+  private baseUrl = 'http://localhost:8080/api/complain'; // Replace with your backend API URL
+
+  constructor(private http: HttpClient) {}
+
+  submitComplain(complainData: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/submit`, complainData);
+  }
+
+  uploadFile(file: FormData): Observable<string> {
+    return this.http.post(`${this.baseUrl}/upload`, file, { responseType: 'text' }) as Observable<string>;
+}
+
+
+
+}
